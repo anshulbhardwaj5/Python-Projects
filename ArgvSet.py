f43b1ebb01400cde1e62516c5cd9cd4278019527
Anshul Bhardwaj <42047360+anshulbhardwaj5@users.noreply.github.com>
2020-04-18 23:37:46 +0530

    Arguments Inputing

---
+++ ArgvSet.py
@@ -0,0 +1,10 @@
+# "import" this is how you add features to your scripts from the python feature set.
+#"argv" is the argument variable	
+from sys import argv 
+
+script , first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "YOur third variable is:", third
