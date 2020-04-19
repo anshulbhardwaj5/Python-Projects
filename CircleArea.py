9df475f2942ecf85e30a20109c5347755d70aed7
Anshul Bhardwaj <42047360+anshulbhardwaj5@users.noreply.github.com>
2020-04-19 23:46:39 +0530

    edit it asap

---
+++ CircleArea.py
@@ -0,0 +1,11 @@
+# Area of a circle Program
+
+# This is the simple one line script to calculate the area of circle
+print ("Area of circle is ", 3.14*float(input("Enter the value of radius"))**2)
+
+#
+radius = float(input("Enter the radius of circle")
+area = 3.14*radius**2
+print ("Area of a circle is", area)
+
+
