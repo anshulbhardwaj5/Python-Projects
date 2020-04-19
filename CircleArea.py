2fb3dd249198987861433ce0fbf3a725b311b5cd
Anshul Bhardwaj <42047360+anshulbhardwaj5@users.noreply.github.com>
2020-04-20 00:05:21 +0530

    Triangle Area Finding

--- CircleArea.py
+++ CircleArea.py
@@ -1,4 +1,12 @@
 # Area of a circle Program
 
 # This is the simple one line script to calculate the area of circle
+
 print ("Area of circle is ", 3.14*float(input("Enter the value of radius"))**2)
+
+# in three line script
+
+radius = int(input("Enter the radius of circle"))
+area =3.14*r**2
+print ("Area of a circle is", area) 
+
