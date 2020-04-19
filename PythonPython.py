be96930a74d8b2994287b75280b82fc5f8251560
Anshul Bhardwaj <42047360+anshulbhardwaj5@users.noreply.github.com>
2020-04-19 23:19:38 +0530

    Triangle Program Py

---
+++ PythonPython.py
@@ -0,0 +1,9 @@
+# Here we are making a simple python triangle program
+print ("Here we are making a triangle of your desired dimensions!!")
+base = int(input("Enter the Base of triangle...")
+height = int(input("Enter the Height of triangle"))
+
+area = 0
+
+print area = 0.5* base * height
+
