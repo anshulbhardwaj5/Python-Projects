bcbe3b6c0a9320323f2a386a2b8439006af7010a
Anshul Bhardwaj <42047360+anshulbhardwaj5@users.noreply.github.com>
2020-04-24 02:36:22 +0530

    Triangle Area Finder

--- TriangleArea.py
+++ TriangleArea.py
@@ -1,9 +1,14 @@
-# Triangle Area Finding
+# Triangle Area Finder
 
 a = float(input("Enter the one side")) # we can use int as it gives same result
 b = float(input("Enter the second side"))
 c = float(input("Enter the third side"))
 s = (a+b+c)/2  # formula to find area
-area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
-print ("The area of triangle is area ", area)
+
+  area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+
+    print ("The area of triangle is area ", area)
+
+
+
 
