8672d030b200ce1c0ab1af8e8d9f0c93cfbee016
Anshul Bhardwaj <42047360+anshulbhardwaj5@users.noreply.github.com>
2020-04-24 03:12:38 +0530

    Fibonacci Numbers

---
+++ Fibonacci Series.py
@@ -0,0 +1,21 @@
+#Printing Fibonacci Numbers 
+
+print("Fibonacci Numbers")
+n = int(input("Enter a number"))
+def fib(n):  
+  a = 0
+  b = 1
+  print(a)
+  print(b)
+  
+  if n < 0:
+    print("Number can't be taken below zero!!") 
+  else:
+        for i in range(2 , n):
+        c = a+b
+        a = b 
+        b = c
+        print c
+fib(n)        
+     
+      
