7eb78f12b96186303a49229593f3ec2aaa80a45b
Anshul Bhardwaj <42047360+anshulbhardwaj5@users.noreply.github.com>
2020-04-25 04:09:38 +0530

    Round and Abs added

--- Round Function.py
+++ Round Function.py
@@ -1,4 +1,28 @@
-# Here we'll play with round function
-x = 2.4
-print (round(x))
+print("Happy coding"
+      
+#round function
 
+MathsMarks = .4     # Value after .4 will round to 0.0
+PhysicsMarks = 6.5  # Value after .5 will round to  1.0
+print(round(MathsMarks))
+print (round(PhysicsMarks))
+
+print "Now we will remove minus sign!!"
+
+#abs means the absolute value
+# positive number - same number will be returned 
+# negative number - postive number will be returned
+      
+Python = -13        # I got -13 in py so here I'm cheating just kidding
+Javamarks= -6       # Same with java marks I can make my marks -ve to +ve hahaha  
+print (abs(Python)) 
+print (abs(javamarks))
+
+
+#Output will be
+-----------------------
+# 6.0     round
+# 7.0     round
+# Now we will remove minus sign!!
+# 13      abs 
+# 6568    abs
