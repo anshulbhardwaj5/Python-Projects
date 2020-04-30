1df4844f959b8fcab208f9c3e8fa744abd5def17
Anshul Bhardwaj <42047360+anshulbhardwaj5@users.noreply.github.com>
2020-04-30 20:31:53 +0530

    date and second updated

---
+++ realdatetimeprogram.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+now = datetime.now()
+
+print(now.date())
+
+print("hrs", now.hour, "Seconds", now.sec)
+
+
+
+
+#Output------------------
+#2020-04-30
+#('hrs', 20, 'Seconds', 30)
