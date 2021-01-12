bfd64da76cfc9e5f2a26530b25344784d453e947
Anshul Sharma <42047360+anshulbhardwaj5@users.noreply.github.com>
2021-01-12 16:13:27 +0530

    comments added

--- FactusingRecursion.py
+++ FactusingRecursion.py
@@ -1,7 +1,8 @@
 def fact(n): 
+    #if n is equals to 0 then factorial will be one!!
     if (n==0):   
-    return 1        # n's value is 1 always 
-    
+    return 1        
+    #If n == 3 then fact will be 6
     return n*fact(n-1) 
     
 result = fact(6)
