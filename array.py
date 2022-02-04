7cb061e798614e263cad531558fd40647d1ac8cf
ipanshul <asharma@ichhapurti.com>
2022-02-05 01:50:32 +0530

    Local|Global|math|datetime Functionality

---
+++ array.py
@@ -0,0 +1,36 @@
+import math
+from my_module import person1
+import datetime
+import platform
+
+
+# def local_scope():
+#     car = "Toyota Crysta"
+    
+#     def inner_local():
+#         print(car)
+#     inner_local()
+    
+#     def in_inner_local():
+#         print("my Inner Local %", %(car))
+#     in_inner_local()
+# local_scope()
+
+# a = my_module.person1["name"]
+# print (person1["age"])
+
+# x = datetime.datetime(2020, 5, 17)
+# print (x)
+# print (x.strftime("%A"+' '+ "%p"+''+"%%"))
+
+
+# x = min(2, 33, 9)
+# y = max(2, 3, 4211)
+
+# print(x)
+# print(y)
+
+
+# x = pow(2, 4)
+x = math.pi()
+print(x)    
---
+++ my_module.py
@@ -0,0 +1,9 @@
+def greeting(name):
+    print("Hello" + name)
+
+person1 = {
+    "name": "Jhon",
+    "name": "Anhul",
+    "age": 23,
+    "Job": "SDE-1"
+}
