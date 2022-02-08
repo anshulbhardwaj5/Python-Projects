83651af3c082fdff2f22e508b3b4a86f57763489
ipanshul <asharma@ichhapurti.com>
2022-02-08 23:52:36 +0530

    array

--- array.py
+++ array.py
@@ -1,36 +1,47 @@
-import math
-from my_module import person1
-import datetime
-import platform
+import json
 
-
-# def local_scope():
-#     car = "Toyota Crysta"
+x = {
     
-#     def inner_local():
-#         print(car)
-#     inner_local()
+}
+
+
+# import math
+
+# from my_module import person1
+# import datetime
+# import platform
+
+
+# # def local_scope():
+# #     car = "Toyota Crysta"
     
-#     def in_inner_local():
-#         print("my Inner Local %", %(car))
-#     in_inner_local()
-# local_scope()
+# #     def inner_local():
+# #         print(car)
+# #     inner_local()
+    
+# #     def in_inner_local():
+# #         print("my Inner Local %", %(car))
+# #     in_inner_local()
+# # local_scope()
 
-# a = my_module.person1["name"]
-# print (person1["age"])
+# # a = my_module.person1["name"]
+# # print (person1["age"])
 
-# x = datetime.datetime(2020, 5, 17)
-# print (x)
-# print (x.strftime("%A"+' '+ "%p"+''+"%%"))
-
-
-# x = min(2, 33, 9)
-# y = max(2, 3, 4211)
-
-# print(x)
-# print(y)
+# # x = datetime.datetime(2020, 5, 17)
+# # print (x)
+# # print (x.strftime("%A"+' '+ "%p"+''+"%%"))
 
 
-# x = pow(2, 4)
-x = math.pi()
-print(x)    
+# # x = min(2, 33, 9)
+# # y = max(2, 3, 4211)
+
+# # print(x)
+# # print(y)
+
+
+# # x = pow(2, 4)
+# x = math.pi()
+# print(x)     
+
+
+
